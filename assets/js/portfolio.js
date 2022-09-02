accc14f31555b886e796b6f8cd220784fe042420
Payam Dowlatyari <pdowlatyari@gmail.com>
2022-09-02 16:42:22 -0700

    update style and links

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,22 +3,12 @@ window.onscroll = () => { scrollFunction() };
 const scrollFunction = () => {
 
     const topNav = document.getElementById('top-nav');
-    const arrow = document.getElementById("hero-arrow");
     const logo = document.getElementById("logo");
     const home = document.getElementById("logo-home");
 
     let navLinks = document.getElementsByClassName('nav-link');
 
-
-
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-
-    // if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) { 
-    //   arrow.style.display = "none";
-    // } else {
-    //   arrow.style.paddingTop = "5vh"
-    // }
-
 
     topNav.style.paddingTop = "10px";
     topNav.style.paddingBottom = "10px";
@@ -34,20 +24,13 @@ const scrollFunction = () => {
 
   } else {
     topNav.style.paddingTop = "5vh";
-    // topNav.style.paddingBottom = "40vh";
-    // topNav.style.height = "100vh";
     topNav.style.transition = "1s";
-    // arrow.style.display = "block";
-    // arrow.style.paddingTop = "0vh"
     logo.style.display = "block";
     home.style.display = "none";
-
-    // topNav.style.backgroundColor = "#17202A";
     topNav.style.backgroundColor = "transparent";
 
     for(let i = 0; i < navLinks.length; i++)
         navLinks[i].style.color = "#ddd"
-    
   }
 }
 
