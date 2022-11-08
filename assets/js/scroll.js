51fe98260b35f0070e6e68ecb9fd01102555eaac
Payam Dowlatyari <pdowlatyari@gmail.com>
2022-11-07 19:23:36 -0800

    New blog design

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,3 +1,5 @@
+// scroll sections
+
 const scrollElements = document.querySelectorAll(".js-scroll");
 
 const elementInView = (el, dividend = 1) => {
@@ -38,3 +40,80 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => { 
   handleScrollAnimation();
 });
+
+// open hanmburger menu
+
+const select = document.getElementById('select-menu');
+const open = document.getElementById('open-menu');
+let c = document.getElementById('close');
+let o = document.getElementById('open');
+
+open.style.display = 'none';
+o.style.display = 'inline';
+c.style.display = 'none';
+
+select.addEventListener('click', () => {
+
+  if(open.style.display == 'none'){
+    o.style.display = 'none';
+    c.style.display = 'inline';
+    open.style.display = 'inline';
+  }  
+
+  else{
+    c.style.display = 'none';
+    o.style.display = 'inline';
+    open.style.display = 'none';
+  }
+});
+
+// social menu
+
+const social = document.getElementById('social-menu');
+const openS = document.getElementById('open-social');
+let cs = document.getElementById('close-s');
+let os = document.getElementById('open-s');
+
+openS.style.display = 'none';
+os.style.display = 'inline';
+cs.style.display = 'none';
+
+social.addEventListener('click', () => {
+
+  if(openS.style.display == 'none'){
+    os.style.display = 'none';
+    cs.style.display = 'inline';
+    openS.style.display = 'inline';
+  }  
+
+  else{
+    cs.style.display = 'none';
+    os.style.display = 'inline';
+    openS.style.display = 'none';
+  }
+})
+
+// Add up arrow
+window.onscroll = () => { scrollFunction() };
+
+const scrollFunction = () => {
+
+const arrow = document.getElementsByClassName('fa-chevron-up');
+
+if (document.documentElement.scrollTop > 10) {
+  arrow[0].style.display = 'inline';
+} else {
+  arrow[0].style.display = 'none';
+}
+
+}
+// IMDB
+
+((d, s, id) => { 
+  let js, stags = d.getElementsByTagName(s)[0];   
+  if (d.getElementById(id)) { return; } 
+  js = d.createElement(s); 
+  js.id = id; 
+  js.src = "https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/js/rating.js"; 
+  stags.parentNode.insertBefore(js, stags); 
+})(document, "script", "imdb-rating-api");
