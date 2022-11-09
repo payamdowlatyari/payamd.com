1119a9bbf67ddafd2d011d1c7532912825580b5c
Payam Dowlatyari <pdowlatyari@gmail.com>
2022-11-09 10:43:26 -0800

    Update description design

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -94,26 +94,26 @@ social.addEventListener('click', () => {
 })
 
 // Add up arrow
-window.onscroll = () => { scrollFunction() };
+// window.onscroll = () => { scrollFunction() };
 
-const scrollFunction = () => {
+// const scrollFunction = () => {
 
-const arrow = document.getElementsByClassName('fa-chevron-up');
+// const arrow = document.getElementsByClassName('fa-chevron-up');
 
-if (document.documentElement.scrollTop > 10) {
-  arrow[0].style.display = 'inline';
-} else {
-  arrow[0].style.display = 'none';
-}
+// if (document.documentElement.scrollTop > 10) {
+//   arrow[0].style.display = 'inline';
+// } else {
+//   arrow[0].style.display = 'none';
+// }
 
-}
+// }
 // IMDB
 
-((d, s, id) => { 
-  let js, stags = d.getElementsByTagName(s)[0];   
-  if (d.getElementById(id)) { return; } 
-  js = d.createElement(s); 
-  js.id = id; 
-  js.src = "https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/js/rating.js"; 
-  stags.parentNode.insertBefore(js, stags); 
-})(document, "script", "imdb-rating-api");
+// ((d, s, id) => { 
+//   let js, stags = d.getElementsByTagName(s)[0];   
+//   if (d.getElementById(id)) { return; } 
+//   js = d.createElement(s); 
+//   js.id = id; 
+//   js.src = "https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/js/rating.js"; 
+//   stags.parentNode.insertBefore(js, stags); 
+// })(document, "script", "imdb-rating-api");
